2aed489ac967a75f775e513a064a3398b0a739a4
07448107357 <140864370+07448107357@users.noreply.github.com>
2026-08-04 18:09:59 +0100

    Change TeleBot token in bot.py
    Updated the bot token for the TeleBot instance.

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 
-bot = telebot.TeleBot ('8920172447:AAFls2T5B3IlAJrVfERcl1PNzREGUbg3TMs')
+bot = telebot.TeleBot ('8920172447:AAFrn1H4vsSJj6FEK30lYbWp75mZ91sbTVM')
 
 
 def calculate_rsi(prices, period=14):
