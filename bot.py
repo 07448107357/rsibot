cd988d6f5d9bf729da2c418c9044b8565bb77ff1
07448107357 <140864370+07448107357@users.noreply.github.com>
2026-08-04 15:30:40 +0100

    Change bot token in bot.py
    Updated the Telegram bot token for security reasons.

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ import numpy as np
 
 
 
-bot = telebot.TeleBot('8490271124:AAFODf12Q6deP72uEz3gapR1hhjB2r1G8lU')
+bot = telebot.TeleBot('8920172447:AAFls2T5B3IlAJrVfERcl1PNzREGUbg3TMs')
 
 
 def calculate_rsi(prices, period=14):
