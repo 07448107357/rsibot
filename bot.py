666a8afdd7eb673a18d21fdfa24cfcc197e83295
07448107357 <140864370+07448107357@users.noreply.github.com>
2026-08-08 11:45:02 +0100

    Add message prompt for user input in bot

--- bot.py
+++ bot.py
@@ -622,6 +622,7 @@ def send_welcome(message):
     
     # إرسال الرسالة مع الأزرار
     bot.send_message(message.chat.id, "اختر الأصل لتحليله واستخراج الإشارة:", reply_markup=markup)
+    
 
 # تشغيل البوت
 if __name__ == "__main__":
