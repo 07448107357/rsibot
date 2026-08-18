84c9efc9093fc44c2266ee64c756e2b7316d217c
07448107357 <140864370+07448107357@users.noreply.github.com>
2026-08-18 21:18:45 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Cont
 # إعداد التسجيل (Logging)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 # 🔑 ضع/ضعي التوكن الخاص بك هنا بين علامات التنصيص
-TOKEN = "8920172447:AAHeKDV4xrnBJmllXjkHp37PWVkm4bPKFWw"
+TOKEN = "8920172447:AAGJXxHcTpK-6q0AovygTNnjaL3ABrEI_P0"
 # ---------------------------------------------------------
 # 1. قائمة الأصول المالية (تم إضافة EUR/GBP OTC)
 # ---------------------------------------------------------
