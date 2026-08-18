8a268af08e5d07546bc8e6958cd9b77d619057c3
07448107357 <140864370+07448107357@users.noreply.github.com>
2026-08-18 19:07:14 +0100

    Change bot token in bot.py
    Updated the bot token for authentication.

--- bot.py
+++ bot.py
@@ -8,7 +8,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Cont
 # إعداد التسجيل (Logging)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 # 🔑 ضع/ضعي التوكن الخاص بك هنا بين علامات التنصيص
-TOKEN = "8920172447:AAErm3oScTsJNnOrQLzmCsmLQxZCckLL26M"
+TOKEN = "8920172447:AAE6jyJoS4c72dnKm30ftNJ_eua-okUuXk4"
 # ---------------------------------------------------------
 # 1. قائمة الأصول المالية (تم إضافة EUR/GBP OTC)
 # ---------------------------------------------------------
