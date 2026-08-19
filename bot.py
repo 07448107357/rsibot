0c61680828994ae27c5da313ac6196db4bcf1122
07448107357 <140864370+07448107357@users.noreply.github.com>
2026-08-19 10:39:22 +0100

    Change bot token in bot.py
    Updated the bot token to a new value.

--- bot.py
+++ bot.py
@@ -10,7 +10,7 @@ from telegram.ext import Application, CommandHandler, CallbackQueryHandler, Cont
 # 1. إعداد التسجيل (Logging)
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 # بدلاً من كتابة التوكن مباشرة، يقرأه البوت من إعدادات Render:
-TOKEN = "8866300939:AAFzrO0uLQXxUAdDlJNG5xqpt6sLQve3AfQ"
+TOKEN = "8866300939:AAFzr00uLQXxUadD1JNG5xqpt6sLQve3AfM"
 # --------------------------------------------------
 # 3. قاموس الأصول الشامل (ASSETS) من الصور
 # --------------------------------------------------
