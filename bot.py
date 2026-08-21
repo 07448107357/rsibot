df4d52cab70c68634ccba456373471aea35d8a2a
07448107357 <140864370+07448107357@users.noreply.github.com>
2026-08-21 10:18:20 +0100

    Update bot token in bot.py

--- bot.py
+++ bot.py
@@ -198,7 +198,7 @@ async def show_signal_page(query, context):
 
 if __name__ == "__main__":
     from telegram.ext import ApplicationBuilder
-    app = ApplicationBuilder().token("8866300939:AAFDciFPyJmWE1zpk9YG5LKECY_SzT2byQQ").build()
+    app = ApplicationBuilder().token("8866300939:AAFDciFpyJMwE1zpk9YG5LKECY_SzT2byQQ").build()
     
     app.add_handler(CommandHandler("start", start))
     app.add_handler(CallbackQueryHandler(start, pattern="^start$"))
