3cd88d4a5f958b5a92c6fe9b5ee37bae8b1ebdfe
07448107357 <140864370+07448107357@users.noreply.github.com>
2026-08-22 04:49:33 +0100

    Fix token formatting in bot.py

--- bot.py
+++ bot.py
@@ -21,8 +21,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 
 # new token
-app = ApplicationBuilder().token("8686410705:AAEIwW4Zbj1NGwzxqOLh2w4aQHGQcxX9ibo
-").build()
+app = ApplicationBuilder().token("8686410705:AAEIwW4Zbj1NGwzxqOLh2w4aQHGQcxX9ibo").build()
 
 
 # --- جميع الأصول والأزواج مرتبة بالرموز والأعلام والملونة ---
