8b2abbafba236c22a80fbe9ce5dd371db665303d
07448107357 <140864370+07448107357@users.noreply.github.com>
2026-08-22 05:42:41 +0100

    Change Telegram bot token
    Updated bot token for the Telegram bot application.

--- bot.py
+++ bot.py
@@ -21,7 +21,7 @@ from telegram import Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import ApplicationBuilder, CommandHandler, CallbackQueryHandler, ContextTypes
 
 # new token
-app = ApplicationBuilder().token("8686410705:AAEqBWawfT7mQr_DWseQcWtB3S8WGwr8rvs").build()
+app = ApplicationBuilder().token("8686410705:AAHR_u4tEEg8veHcgreGCvZbozQHZxzbufg").build()
 
 
 # --- جميع الأصول والأزواج مرتبة بالرموز والأعلام والملونة ---
